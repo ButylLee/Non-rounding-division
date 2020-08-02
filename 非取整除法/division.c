7eed7d25772fdf94f0d4115801bd37391a78f9ea
James Taylor <james.taylor_rw@outlook.com>
2020-08-02 12:52:00 +0800

    Formatting

--- 非取整除法/division.c
+++ 非取整除法/division.c
@@ -13,20 +13,24 @@ int main()
 
 
 	char* decimal = NULL;							//创建变长数组，多一位用于四舍五入，使用char储存一位数以节省空间
-	if ((decimal = (char*)calloc(precision + 1, sizeof(char))) == NULL) {
+	if ((decimal = (char*)calloc(precision + 1, sizeof(char))) == NULL)
+	{
 		printf("memory allocated failed!");
 		exit(-1);
 	}
 
 	quotient = dividend / divisor;
 	remainder = dividend % divisor;
-	if (quotient < 0)divisor *= -1;								//商为负数的处理
-	for (unsigned i = 0; i < precision + 1; i++) {
+	if (quotient < 0)
+		divisor *= -1;								//商为负数的处理
+	for (unsigned i = 0; i < precision + 1; i++)
+	{
 		remainder *= 10;										/****核心步骤****/
 		decimal[i] = remainder / divisor;						/****核心步骤****/
 		remainder %= divisor;									/****核心步骤****/
 
-		if (!IS_END_ZERO && remainder == 0) {					/***除尽时末尾不保留0的处理***/
+		if (!IS_END_ZERO && remainder == 0)						/***除尽时末尾不保留0的处理***/
+		{
 			if (precision != i)									//判断除尽时数组长度是否 不能 刚好容纳最后一位非0数
 				precision = i + 1;
 			break;
@@ -34,10 +38,12 @@ int main()
 	}
 
 	/***四舍五入***/
-	if (isRound && decimal[precision] > 4) {
+	if (isRound && decimal[precision] > 4)
+	{
 		int i = 0;
 		do {
-			if (i)decimal[precision - i] = 0;					//将为10的上一位置0
+			if (i != 0)
+				decimal[precision - i] = 0;						//将为10的上一位置0
 			i++;
 			precision - i + 1
 				? decimal[precision - i]++
@@ -48,7 +54,8 @@ int main()
 	printf("%d", quotient);
 	if (precision != 0)
 		putchar('.');
-	for (unsigned i = 0; i < precision; i++) {
+	for (unsigned i = 0; i < precision; i++)
+	{
 		printf("%d", decimal[i]);
 	}
 
